f0efa5d746c886db67b9cc2baddb1f630b98dfdd
Oleksandr Havryliuk <deolis.ua@gmail.com>
2023-09-26 18:55:32 +0300

    update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -3,3 +3,14 @@ import { useDynamicAdapt } from './modules/dynamicAdapt';
 
 burgerMenu();
 useDynamicAdapt();
+
+// add styles to header on scroll
+const header = document.querySelector('.header');
+
+window.addEventListener('scroll', () => {
+  if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
+    header.classList.add('header--shadow');
+  } else {
+    header.classList.remove('header--shadow');
+  }
+});
