9e3562a1d14b511bd20b09ee4bad1027eb8957f4
Oleksandr Havryliuk <deolis.ua@gmail.com>
2023-09-28 16:45:58 +0300

    update webpack.config.js

--- gulp/config/webpack.config.js
+++ gulp/config/webpack.config.js
@@ -5,6 +5,7 @@ const config = {
     app: './src/js/app.js',
     index: './src/js/index.js',
     services: './src/js/services.js',
+    about: './src/js/about.js',
   },
   output: {
     filename: '[name].min.js',
